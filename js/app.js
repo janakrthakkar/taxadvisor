faff7114d419509ef061003bf2b152f114943773
janakrthakkar <72696703+janakrthakkar@users.noreply.github.com>
2020-12-28 18:00:12 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,10 +10,3 @@ hamburgerMenu.addEventListener("click", () => {
 function closeNav() {
   header.classList.remove("active");
 }
-
-//intro
-const tl = gsap.timeline({ defaults: { ease: "power1.Inout" } });
-
-tl.to(".intro-text", { y: "0%", duration: 1, stagger: 0.4 });
-tl.to(".intro-slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
